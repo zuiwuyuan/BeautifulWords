3647e5ea65602bb70841276d1ee52fca8eb9f7bf
zuiwuyuan <1936900253@qq.com>
2016-11-06 21:06:30 +0800

    delete 内存监控

--- sample/src/main/java/com/chenyuan/sentence/mvp/ui/common/BaseActivity.java
+++ sample/src/main/java/com/chenyuan/sentence/mvp/ui/common/BaseActivity.java
@@ -4,9 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-import com.chenyuan.sentence.app.MyApp;
 import com.chenyuan.sentence.common.ActivityController;
-import com.squareup.leakcanary.RefWatcher;
 
 
 public class BaseActivity extends AppCompatActivity {
@@ -14,10 +12,6 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //在自己的应用初始Activity中加入如下两行代码
-        RefWatcher refWatcher = MyApp.getRefWatcher(this);
-        refWatcher.watch(this);
 
         ActivityController.addActivity(this);
 
--- sample/src/main/java/com/chenyuan/sentence/mvp/ui/fragment/BaseFragment.java
+++ sample/src/main/java/com/chenyuan/sentence/mvp/ui/fragment/BaseFragment.java
@@ -3,20 +3,9 @@ package com.chenyuan.sentence.mvp.ui.fragment;
 
 import android.support.v4.app.Fragment;
 
-import com.chenyuan.sentence.app.MyApp;
-import com.squareup.leakcanary.RefWatcher;
-
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BaseFragment extends Fragment {
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-
-        RefWatcher refWatcher = MyApp.getRefWatcher(getActivity());
-        refWatcher.watch(this);
-    }
 
 }
